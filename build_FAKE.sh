5e0b8e0a43efc56829934fdba6eb28dbafce1857
Kent Boogaart <kent.boogaart@gmail.com>
2017-10-23 11:50:11 +1030

    Remove old FAKE stuff

--- build_FAKE.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mono .nuget/nuget.exe install FAKE -Version 5.0.0-beta005 -Prerelease -OutputDirectory "Src/packages" -ExcludeVersion
-mono Src/packages/FAKE/tools/FAKE.exe build.fsx $@
