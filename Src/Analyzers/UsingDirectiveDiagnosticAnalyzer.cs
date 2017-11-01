eae36dbf4b0442606b656c57839f57f0209e0587
Kent Boogaart <kent.boogaart@gmail.com>
2017-11-01 16:34:13 +1030

    feature: using directives should be sorted correctly

--- Src/Analyzers/UsingDirectiveDiagnosticAnalyzer.cs
+++ Src/Analyzers/UsingDirectiveDiagnosticAnalyzer.cs
@@ -3,12 +3,14 @@
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public sealed class UsingDirectiveDiagnosticAnalyzer : DiagnosticAnalyzer
     {
         public const string UsingsWithinNamespaceDiagnosticId = "CH0002";
+        public const string UsingsSortedCorrectlyDiagnosticId = "CH0003";
 
         private static DiagnosticDescriptor UsingsWithinNamespaceRule = new DiagnosticDescriptor(
             UsingsWithinNamespaceDiagnosticId,
@@ -18,15 +20,25 @@
             DiagnosticSeverity.Warning,
             isEnabledByDefault: true);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(UsingsWithinNamespaceRule);
+        private static DiagnosticDescriptor UsingsSortedCorrectlyRule = new DiagnosticDescriptor(
+            UsingsSortedCorrectlyDiagnosticId,
+            "Using directives must be sorted correctly",
+            "Sort using directives in alphabetical order, with System usings first.",
+            "Style",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(UsingsWithinNamespaceRule, UsingsSortedCorrectlyRule);
 
         public override void Initialize(AnalysisContext context)
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-            context.RegisterSyntaxNodeAction(AnalyzeUsingDirective, ImmutableArray.Create(SyntaxKind.UsingDirective));
+
+            context.RegisterSyntaxNodeAction(EnsureUsingDirectiveIsWithinNamespaceDeclaration, ImmutableArray.Create(SyntaxKind.UsingDirective));
+            context.RegisterSyntaxNodeAction(EnsureUsingDirectivesAreSortedCorrectly, ImmutableArray.Create(SyntaxKind.CompilationUnit, SyntaxKind.NamespaceDeclaration));
         }
 
-        private static void AnalyzeUsingDirective(SyntaxNodeAnalysisContext context)
+        private static void EnsureUsingDirectiveIsWithinNamespaceDeclaration(SyntaxNodeAnalysisContext context)
         {
             var node = context.Node;
             var parent = node.Parent;
@@ -37,6 +49,34 @@
                     UsingsWithinNamespaceRule,
                     node.GetLocation());
                 context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static void EnsureUsingDirectivesAreSortedCorrectly(SyntaxNodeAnalysisContext context)
+        {
+            var node = context.Node;
+            SyntaxList<UsingDirectiveSyntax> usingDirectives;
+
+            if (node.IsKind(SyntaxKind.CompilationUnit))
+            {
+                usingDirectives = ((CompilationUnitSyntax)node).Usings;
+            }
+            else
+            {
+                usingDirectives = ((NamespaceDeclarationSyntax)node).Usings;
+            }
+
+            for (var i = 0; i < usingDirectives.Count - 1; ++i)
+            {
+                var compare = UsingDirectiveComparer.Instance.Compare(usingDirectives[i], usingDirectives[i + 1]);
+
+                if (compare > 0)
+                {
+                    var diagnostic = Diagnostic.Create(
+                        UsingsSortedCorrectlyRule,
+                        usingDirectives[i].GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
             }
         }
     }
