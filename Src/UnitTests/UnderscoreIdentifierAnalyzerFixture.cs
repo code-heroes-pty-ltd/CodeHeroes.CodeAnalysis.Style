d6348814d83ffe540eae2298f640ae732de1a800
Kent Boogaart <kent.boogaart@gmail.com>
2017-11-03 13:05:53 +1030

    feature: underscore identifiers reserved for discard semantics

---
+++ Src/UnitTests/UnderscoreIdentifierAnalyzerFixture.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeHeroes.CodeAnalysis.Style.UnitTests
+{
+    using System.Collections.Generic;
+    using CodeHeroes.CodeAnalysis.Style.UnitTests.TestHelper;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Xunit;
+
+    public sealed class UnderscoreIdentifierAnalyzerFixture : CodeFixVerifier
+    {
+        [Theory]
+        [MemberData(nameof(CH0006Diagnostics))]
+        public void ch0006_diagnoses_correctly(string[] sources, DiagnosticResult[] diagnosticResults)
+        {
+            this.VerifyCSharpDiagnostic(sources, diagnosticResults);
+        }
+
+        public static IEnumerable<object[]> CH0006Diagnostics =>
+            GetDataForDiagnosticVerification(UnderscoreIdentifierAnalyzer.DiagnosticId);
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+            new UnderscoreIdentifierAnalyzer();
+    }
+}
