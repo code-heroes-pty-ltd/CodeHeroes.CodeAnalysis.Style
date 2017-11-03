f7a9298da0b1e0e377f229cd2a4b66d693e7cdae
Kent Boogaart <kent.boogaart@gmail.com>
2017-11-03 18:23:11 +1030

    post-release fixes

--- Src/Analyzers/UsingDirectiveComparer.cs
+++ Src/Analyzers/UsingDirectiveComparer.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
 
     public sealed class UsingDirectiveComparer : IComparer<UsingDirectiveSyntax>
@@ -16,6 +18,22 @@
         {
             var xName = x.Name.ToString();
             var yName = y.Name.ToString();
+            var xIsAliased = IsAliased(x);
+            var yIsAliased = IsAliased(y);
+
+            if (xIsAliased != yIsAliased)
+            {
+                return xIsAliased.CompareTo(yIsAliased);
+            }
+
+            var xIsStatic = IsStatic(x);
+            var yIsStatic = IsStatic(y);
+
+            if (xIsStatic != yIsStatic)
+            {
+                return xIsStatic.CompareTo(yIsStatic);
+            }
+
             var xSegment = IsSystem(xName) ? 0 : 1;
             var ySegment = IsSystem(yName) ? 0 : 1;
 
@@ -24,8 +42,21 @@
                 return xSegment.CompareTo(ySegment);
             }
 
-            return xName.CompareTo(yName);
+            if (xIsAliased)
+            {
+                return x.Alias.Name.ToString().CompareTo(y.Alias.Name.ToString());
+            }
+            else
+            {
+                return xName.CompareTo(yName);
+            }
         }
+
+        private static bool IsAliased(UsingDirectiveSyntax usingDirectiveSyntax) =>
+            usingDirectiveSyntax.Alias != null;
+
+        private static bool IsStatic(UsingDirectiveSyntax usingDirectiveSyntax) =>
+            !usingDirectiveSyntax.StaticKeyword.IsKind(SyntaxKind.None);
 
         private static bool IsSystem(string usingDirective) =>
             usingDirective.StartsWith("System", StringComparison.Ordinal);
