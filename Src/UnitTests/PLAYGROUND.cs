e2586ee0f52deff7f42c91a3863f0d9743880d3b
Kent Boogaart <kent.boogaart@gmail.com>
2017-11-03 13:03:23 +1030

    feature: underscore identifiers reserved for discard semantics

--- Src/UnitTests/PLAYGROUND.cs
+++ Src/UnitTests/PLAYGROUND.cs
@@ -1,34 +1,23 @@
 ﻿//using System;
 
-//namespace Foo
+//class Foo
 //{
-//    using System.Collections;
-
-//    public class Foo
+//    void Bar()
 //    {
-//        public class Baz
+//        Func<string, int> valid = _ => 42;
+//        Func<string, int> invalid = _ => _.Length;
+
+//        Func<string, string, string, int> valid2 = (_, __, ___, ____) => 42;
+//        Func<string, string, string, int> invalid2 = (a, b, c, _) => _.Length;
+
+//        Func<string, int> invalid3 = _ =>
 //        {
+//            if (DateTime.Now.Year == 2017)
+//            {
+//                return _.Length;
+//            }
 
-//        }
+//            return 32;
+//        };
 //    }
-
-//    public class Bar
-//    {
-
-//    }
-
-//    public interface IFoo
-//    {
-
-//    }
-
-//    public struct Shar
-//    {
-
-//    }
-//}
-
-//public class Fiz
-//{
-
 //}
